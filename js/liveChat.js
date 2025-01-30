c0dec7430070cc4a3907a4ba063f1ee5d33a9863
JK82NL <jk_dance@hotmail.com>
2025-01-30 14:18:59 +0100

    live chat start

---
+++ js/liveChat.js
@@ -0,0 +1,11 @@
+// Global Var's
+
+// Function to sent chat messages
+function getName() {
+    document.getElementById("")
+    // Change button input to 
+}
+
+function chatSend() {
+    document.getElementById("")
+}
